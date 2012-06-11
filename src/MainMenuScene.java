bdd5d89e156a1a839e0b6175b198f23522b0f5db
Guangya <guangyait@gmail.com>
2012-06-11 18:38:37 +0800

    以Menu.java类的对象取代 原主菜单界面类中使用到的菜单项，达到对菜单项的封装，方便于对菜单项的管理

--- src/MainMenuScene.java
+++ src/MainMenuScene.java
@@ -3,46 +3,62 @@ import javax.microedition.lcdui.Image;
 import javax.microedition.lcdui.game.GameCanvas;
 import javax.microedition.lcdui.game.Sprite;
 
+/**
+ * 游戏主菜单，玩家在不同界面切换的窗口
+ * 
+ * @author guangya
+ * @email  guangyait@gmail.com
+ *
+ */
 public class MainMenuScene extends Scene{
 	
-	// 场景尺寸
-	private int width;
-	private int height;
-	// 场景左上角相对于屏幕左上角的坐标
-	private int posX;
-	private int posY;
 	// 功能选择菜单使用的图片及其尺寸
-	private Image btnImage;
 	private int btnWidth;
 	private int btnHeight;
 	// 菜单标题文字
 	private Image selectionImage;
-	// 存储各菜单项使用的图片绘制的坐标。items[菜单项索引][0:菜单图片的坐标，1:菜单文本的坐标][0:x坐标；1:y坐标]
-	private int items[][][];
 	// 当前选中的菜单想的索引
 	private int selectedIndex;      
 	// 菜单项总数常量，使用的目的是减少获取菜单项数组长度的次数
-	private final int MENU_ITEM_NUM = 5;  
-	// 菜单项的说明文字
-	private String itemNames[] = {"开 始 游 戏 ", "开 启 音 效", "操 作 说 明", "关          于", "退 出 游 戏"};
-	// 菜单项对应的游戏状态，与itemNames[]对应
-	private int    itemToGameState[] = {IEngine.GAME_STATE_RUN, -1, IEngine.GAME_STATE_OPERATION, IEngine.GAME_STATE_ABOUT, IEngine.GAME_STATE_EXIT};
-	
+	private final int MENU_ITEM_NUM = 5;  	
+	// 菜单项
+	private Menu menuItems[];
 	
 	public MainMenuScene(IEngine engine, int width, int height){
 		super(engine, width, height);
-		
-		this.width = width;
-		this.height = height;
 
 		try{
-			btnImage = Image.createImage("/image/buttons.png");
 			selectionImage = Image.createImage("/image/selection.png");
 		}catch(Exception e){}
 		
-		btnWidth = btnImage.getWidth();
-		btnHeight = btnImage.getHeight() / 2;
-		initMenuItems();
+		// 菜单项的说明文字
+		String itemNames[] = {"开 始 游 戏 ", "开 启 音 效", "操 作 说 明", "关          于", "退 出 游 戏"};
+		// 菜单项对应的游戏状态，与itemNames[]对应
+		int    itemToGameState[] = {IEngine.GAME_STATE_RUN, -1, IEngine.GAME_STATE_OPERATION, IEngine.GAME_STATE_ABOUT, IEngine.GAME_STATE_EXIT};
+		Image menuItemBg;
+		Image menuItemSBg;
+		Image btnImage;
+		
+		menuItems = new Menu[MENU_ITEM_NUM];
+		
+		try{
+			btnImage = Image.createImage("/image/buttons.png");
+			
+			btnWidth = btnImage.getWidth();
+			btnHeight = btnImage.getHeight() / 2;
+			
+			menuItemBg = Image.createImage(btnImage, 0, 0, btnWidth, btnHeight, Sprite.TRANS_NONE);
+			menuItemSBg = Image.createImage(btnImage, 0, btnHeight, btnWidth, btnHeight, Sprite.TRANS_NONE);
+			
+			for(int i = 0; i < MENU_ITEM_NUM; i++){
+				menuItems[i] = new Menu(itemNames[i]);
+				menuItems[i].setBoundEvent(itemToGameState[i]);
+				menuItems[i].setBackground(menuItemBg);
+				menuItems[i].setSelectedBackground(menuItemSBg);				
+			}
+			calculateMenuItemsPos();
+			menuItems[0].setState(Menu.MENU_ITEM_STATE_SELECTED);
+		}catch(Exception e){}
 	}
 	
 	/**
@@ -55,7 +71,7 @@ public class MainMenuScene extends Scene{
 		this.posX = x;
 		this.posY = y;
 		// 重新计算各元素的坐标
-		initMenuItems();
+		calculateMenuItemsPos();
 	}
 	
 	/**
@@ -66,26 +82,30 @@ public class MainMenuScene extends Scene{
 	public void keyEvent(int gameAction){
 		switch(gameAction){
 		case GameCanvas.UP:
+			menuItems[selectedIndex].setState(Menu.MENU_ITEM_STATE_NORMAL);
 			selectedIndex--;
 			if(selectedIndex < 0){
 				selectedIndex += MENU_ITEM_NUM;
 			}
+			menuItems[selectedIndex].setState(Menu.MENU_ITEM_STATE_SELECTED);
 			break;
 		case GameCanvas.DOWN:
+			menuItems[selectedIndex].setState(Menu.MENU_ITEM_STATE_NORMAL);
 			selectedIndex++;
 			selectedIndex %= MENU_ITEM_NUM;
+			menuItems[selectedIndex].setState(Menu.MENU_ITEM_STATE_SELECTED);
 			break;
 		case GameCanvas.FIRE:
 			if(1 == selectedIndex){
-				if(itemNames[1] == "开 启 音 效"){
-					itemNames[1] = "关 闭 音 效";
+				if(menuItems[selectedIndex].getText() == "开 启 音 效"){
+					menuItems[selectedIndex].setText("关 闭 音 效");
 					engine.setSoundSwitch(false);
 				}else{
-					itemNames[1] = "开 启 音 效";
+					menuItems[selectedIndex].setText("开 启 音 效");
 					engine.setSoundSwitch(true);
 				}
 			}else{
-				engine.setGameState(itemToGameState[selectedIndex]);
+				engine.setGameState(menuItems[selectedIndex].getBoundEvent());
 			}
 			break;
 		}
@@ -101,12 +121,7 @@ public class MainMenuScene extends Scene{
 		gra.drawImage(selectionImage, 64, 40, Graphics.LEFT|Graphics.TOP);
 		
 		for(int i = 0; i < MENU_ITEM_NUM; i++){
-			if(selectedIndex == i){
-				gra.drawRegion(btnImage, 0, btnHeight, btnWidth, btnHeight, Sprite.TRANS_NONE, items[i][0][0], items[i][0][1], Graphics.LEFT|Graphics.TOP);
-			}else{
-				gra.drawRegion(btnImage, 0, 0, btnWidth, btnHeight, Sprite.TRANS_NONE, items[i][0][0], items[i][0][1], Graphics.LEFT|Graphics.TOP);
-			}
-			gra.drawString(itemNames[i], items[i][1][0], items[i][1][1], Graphics.LEFT|Graphics.TOP);
+			menuItems[i].paint(gra);
 		}
 	}
 	
@@ -116,21 +131,12 @@ public class MainMenuScene extends Scene{
 	public void menuEvent(int menu) {}
 	
 	/**
-	 * 初始化菜单项
-	 * 
-	 * 该函数不会重复初始化菜单项
-	 * 
-	 * @return void
+	 * 计算各菜单项在菜单界面的坐标
 	 */
-	private void initMenuItems(){
-		items = new int[MENU_ITEM_NUM][2][2];
+	private void calculateMenuItemsPos(){
+		int menuCenterPosX = posX + canvasWidth / 2;
 		for(int i = 0; i < MENU_ITEM_NUM; i++){
-			items[i][0][0] = posX + (width - btnWidth) / 2;
-			items[i][0][1]= posY + 110 + i * (btnHeight + 5) - btnHeight / 2;
-			items[i][1][0] = items[i][0][0] + 17;
-			items[i][1][1]= items[i][0][1] + 2;
+			menuItems[i].setPosition(menuCenterPosX, posY + 110 + i*(btnHeight + 5));
 		}
-		
-		selectedIndex = 0;
 	}
 }
